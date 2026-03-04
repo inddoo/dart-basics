837d987dd93bd6e3e2a1cf13866734a04e464c4d
inddoo <watashiindonesiana@gmail.com>
2026-03-04 15:12:50 +0700

    perulangan for

--- dart_project/bin/dart_project.dart
+++ dart_project/bin/dart_project.dart
@@ -1,21 +1,7 @@
 import 'dart:io';
 
 void main(){
-    int posSaatIni = 10;
-    switch(posSaatIni){
-        case 1:
-        print("Makan Snack");
-            break;
-        case 2:
-        print("Makan Daging");
-            break;
-        case 3:
-        print("Makan Sayur");
-            break;
-        case 4:
-        print("Berbahagia");
-            break;
-        default:
-        print("Pos yang anda masukkan tidak terdaftar");
-    }
+for(var=1;i<=10;i++>){
+    print("Number $i");
+}
 }
