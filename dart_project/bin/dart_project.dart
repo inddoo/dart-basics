711e22413c12fbceca1b50cba28b843047b52eed
inddoo <watashiindonesiana@gmail.com>
2026-03-04 15:01:24 +0700

    dynamic

--- dart_project/bin/dart_project.dart
+++ dart_project/bin/dart_project.dart
@@ -1,9 +1,7 @@
 import 'package:dart_project/dart_project.dart' as dart_project;
 
 void main() {
-    Map<String, dynamic> data = {
-        ‘url’: https://unisnu.ac.id/’,
-        ‘topic’: ‘Flutter Engineering’,
-    };
-    print(data);
+    dynamic a = 12.2;
+    a = ‘Hello Khanif!’;
+    print(a);
 }
