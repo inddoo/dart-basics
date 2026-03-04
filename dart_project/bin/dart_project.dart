4ba15d33ea4b475079dd724cb1cac73d1c7b1cbf
inddoo <watashiindonesiana@gmail.com>
2026-03-04 15:06:47 +0700

    operator relasional

--- dart_project/bin/dart_project.dart
+++ dart_project/bin/dart_project.dart
@@ -1,6 +1,7 @@
 import 'package:dart_project/dart_project.dart' as dart_project;
 
 void main() {
-   int c = 6;
-c = c+ 6; // c = 12
+   String kota = ‘Jepara’;
+  if(kota == ‘Jepara’) true
+else false;
 }
