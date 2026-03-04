b1ff1d3f4cd68d922f52ddba10077906a2a63e16
inddoo <watashiindonesiana@gmail.com>
2026-03-04 15:05:24 +0700

    operator aritmatika

--- dart_project/bin/dart_project.dart
+++ dart_project/bin/dart_project.dart
@@ -1,7 +1,6 @@
 import 'package:dart_project/dart_project.dart' as dart_project;
 
 void main() {
-   var umur = 18;
-dynamic nama = ‘khanif;
-String kota = ‘Jepara’;
+   int c = 6;
+c = c+ 6; // c = 12
 }
