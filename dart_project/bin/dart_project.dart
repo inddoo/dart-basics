dc1bb11f4f24e1d7615ea79cc72f7ef3f958c738
inddoo <watashiindonesiana@gmail.com>
2026-03-04 15:02:24 +0700

    obj

--- dart_project/bin/dart_project.dart
+++ dart_project/bin/dart_project.dart
@@ -1,7 +1,6 @@
 import 'package:dart_project/dart_project.dart' as dart_project;
 
 void main() {
-    dynamic a = 12.2;
-    a = ‘Hello Khanif!’;
-    print(a);
+    Object object = "Flutter Engineering";
+    print(object);
 }
