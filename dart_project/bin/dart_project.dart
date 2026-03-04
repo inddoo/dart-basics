b5953fa7eea9471c427c77d546f7fbe2ce606039
inddoo <watashiindonesiana@gmail.com>
2026-03-04 15:08:19 +0700

    operator logika

--- dart_project/bin/dart_project.dart
+++ dart_project/bin/dart_project.dart
@@ -1,7 +1,7 @@
 import 'package:dart_project/dart_project.dart' as dart_project;
 
 void main() {
-   String kota = ‘Jepara’;
-  if(kota == ‘Jepara’) true
-else false;
+  var A = true;
+  var B = false;
+  var expr = A && B; //false
 }
